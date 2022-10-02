bded2dc0f144f1fdf9719225c74afe36fcf3b5e0
Mousamia <87888309+Mousamia@users.noreply.github.com>
2022-10-03 01:02:36 +0600

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -4,6 +4,6 @@ const { defineConfig } = require('@vue/cli-service')
 
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
-    ? '/my-project/'
+    ? '/Employee-Crud-Vue/'
     : '/'
 }
